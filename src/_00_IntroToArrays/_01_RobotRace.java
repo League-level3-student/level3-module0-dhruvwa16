81be1c21bf0fefce79d2594a34f10591661091c5
dhruvwa16 <league@iMac-2.attlocal.net>
2018-08-11 17:26:51 -0700

    Robot race

--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -12,14 +12,47 @@ public class _01_RobotRace {
 		//2. create an array of 5 robots.
 			Robot[] robots = new Robot[5];
 		//3. use a for loop to initialize the robots.
-				for (int i = 0; i < robots.length; i++) {
-				
-				}
+			for (int i = 0; i < robots.length; i++) {
+				robots[i] = new Robot();
+				robots[i].setX(100*i+230);
+				robots[i].setY(520);
+			}
+					
+
 			//4. make each robot start at the bottom of the screen, side by side, facing up
 	
 		//5. use another for loop to iterate through the array and make each robot move 
-	    //   a random amount less than 50.
-    	
+	    //   a random amount
+				
+					
+					
+			for (int i = 0; i < 100; i++) {
+				boolean run = true;
+				if(robots[i].getY() <0) {
+					run = false;
+				}
+				while(run == true) {
+					Random r = new Random();
+					int num = r.nextInt(401);
+					robots[i].move(num);
+					i++;
+					if(robots[i].getY() <0) {
+						run = false;
+					}
+					i++;
+					if(i==4) {
+						i=0;
+					}
+
+					if(robots[i].getY() <0) {
+						run = false;
+					
+				}
+				int a = i-1;
+				System.out.println("Robot" +a );
+
+			}
+	
 		//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
 		//7. declare that robot the winner and throw it a party!
@@ -28,5 +61,6 @@ public class _01_RobotRace {
     	
 	    //9. make the robots race around a circular track.
 
+}
 }
 }
