eb4e4d69aecec021704c78e36a3bb8b6b1dd1690
Rohan890 <league@WTS7.attlocal.net>
2018-09-08 17:24:47 -0700

    finished module 0

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -1,9 +1,12 @@
 package _03_More_Array_Fun;
 
+import java.util.HashMap;
+import java.util.Random;
+
 public class MoreArrayFun {
 	// 1. Create a main method to test the other methods you write.
 	public static void main(String[] args) {
-		new MoreArrayFun().method2();
+		new MoreArrayFun().method3();
 	}
 
 	// 2. Write a method that takes an array of Strings and prints all the Strings
@@ -48,10 +51,37 @@ public class MoreArrayFun {
 		for (int i = 0; i < names.length; i += 2) {
 			System.out.println(names[i]);
 		}
+	}
 		// 5. Write a method that takes an array of Strings and prints all the Strings
 		// in the array
 		// in a completely random order. Almost every run of the program should result
 		// in a different order.
-
+		void method3() {
+			int num2 = 0;
+			String[] names = new String[5];
+			names[0] = "Matt";
+			names[1] = "Matt1";
+			names[2] = "Matt2";
+			names[3] = "Matt3";
+			names[4] = "Matt4";
+			HashMap<Integer, String> hash = new HashMap();
+			hash.put(0, "hi2");
+			hash.put(1, "hi2");
+			hash.put(2, "hi2");
+			hash.put(3, "hi2");
+			hash.put(4, "hi2");
+				for (int i = 0; i < names.length; i++) {
+					Random r = new  Random();
+					int num = r.nextInt(4);
+					hash.put(num, "hi");
+					System.out.println(names[num]);
+						
+						
+						}
+		
+					
+				}
+			
+				
+			
 	}
-}
